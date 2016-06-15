0b1d13303a6cac29f7cb396598474a960efb3193
Jérémy JOURDIN <jeremy.jourdin@free.fr>
2016-06-15 23:00:28 +0200

    Fix

--- providers/sync_gateway.rb
+++ providers/sync_gateway.rb
@@ -8,7 +8,7 @@ use_inline_resources
 action :install do
     arch = node['kernel']['machine']
     id   = ""
-    id   = "-{new_resource.release_id}" unless new_resource.release_id.nil?
+    id   = "-#{new_resource.release_id}" unless new_resource.release_id.nil?
 
     package_file = "couchbase-sync-gateway-#{new_resource.edition}_#{new_resource.version}#{id}_#{arch}.deb"
     url_base     = "http://packages.couchbase.com/releases/couchbase-sync-gateway/#{new_resource.version}"
