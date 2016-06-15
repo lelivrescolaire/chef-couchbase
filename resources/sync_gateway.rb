9e173097a3705c0b8711f91afcf0f717f64f2b4d
Jérémy JOURDIN <jeremy.jourdin@free.fr>
2016-06-15 22:42:49 +0200

    Fix

--- resources/sync_gateway.rb
+++ resources/sync_gateway.rb
@@ -7,7 +7,7 @@ actions :install
 
 attribute :version,    :kind_of => String,  :default => '1.2.1'
 attribute :edition,    :kind_of => String,  :default => 'community'
-attribute :release_id, :kind_of => Integer, :défault => 4
+attribute :release_id, :kind_of => Integer, :default => 4
 
 def initialize(*args)
   super
