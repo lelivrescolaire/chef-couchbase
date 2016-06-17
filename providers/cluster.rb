11cd6b0b352382767a5b93be1526f243fbfe861f
Jérémy JOURDIN <jeremy.jourdin@free.fr>
2016-06-17 17:17:55 +0200

    Check if instance already in cluster before join

--- providers/cluster.rb
+++ providers/cluster.rb
@@ -40,24 +40,29 @@ end
 
 action :join do
   Chef::Log.info "Trying to join cluster #{new_resource.cluster_ip}"
-  unless new_resource.ip == new_resource.cluster_ip
-    Chef::Log.info "#{new_resource.cluster_ip} is not the current instance"
 
-    cmd = command('server-add', new_resource.cluster_ip)
-    cmd = couchbase_cli_server_add(cmd, new_resource.ip, new_resource.port, new_resource.username, new_resource.password)
-    cmd = couchbase_cli_services(cmd, new_resource.services)
+  unless check_in_cluster(new_resource.username, new_resource.password, "#{new_resource.cluster_ip}:#{new_resource.port}")
+    unless new_resource.ip == new_resource.cluster_ip
+      Chef::Log.info "#{new_resource.cluster_ip} is not the current instance"
 
-    execute "joining to cluster with #{cmd}" do
-      command cmd
+      cmd = command('server-add', new_resource.cluster_ip)
+      cmd = couchbase_cli_server_add(cmd, new_resource.ip, new_resource.port, new_resource.username, new_resource.password)
+      cmd = couchbase_cli_services(cmd, new_resource.services)
+
+      execute "joining to cluster with #{cmd}" do
+        command cmd
+      end
+
+      couchbase_cluster "default" do
+        install_path new_resource.install_path
+        cluster_ip   new_resource.cluster_ip
+        username     new_resource.username
+        password     new_resource.password
+        action       :rebalance
+      end
     end
-
-    couchbase_cluster "default" do
-      install_path new_resource.install_path
-      cluster_ip   new_resource.cluster_ip
-      username     new_resource.username
-      password     new_resource.password
-      action       :rebalance
-    end
+  else
+    Chef::Log.info "Instance is already member of cluster #{new_resource.cluster_ip}"
   end
 end
 
