909fd0850d8f36162541d917b115dd9c8cf01f50
Jérémy JOURDIN <jeremy.jourdin@free.fr>
2016-06-20 15:40:53 +0200

    Bucket configuration

--- recipes/bucket_configure.rb
+++ recipes/bucket_configure.rb
@@ -3,30 +3,23 @@
 # Recipe:: bucket_configure
 #
 
-common = {
-    "install_path"     => node['couchbase']['server']['paths']['root']
-    "cluster_username" => node['couchbase']['username']
-    "cluster_password" => node['couchbase']['password']
-    "cluster_port"     => node['couchbase']['port']
-    "action"           => :create
-}
-
 node['couchbase']['bucket'].each do |key, config|
-    conf = {}
-    conf.merge(common)
-    conf.merge({
-        "name"            => key
-        "type"            => config['type']
-        "ramsize"         => config['ramsize']
-        "replica"         => config['replica']
-        "priority"        => config['priority']
-        "password"        => config['password']
-        "eviction_policy" => config['eviction_policy']
-        "port"            => config['port']
-        "flush"           => config['flush']
-        "index_replica"   => config['index_replica']
-        "priority"        => config['priority']
-    })
-
-    Chef::Resource::CouchbaseBucket.new key, conf
+    couchbase_bucket "#{key}" do
+#        name             key
+#        type             config['type']
+        ramsize          config['ramsize']
+#        replica          config['replica']
+#        priority         config['priority']
+#        password         config['password']
+#        eviction_policy  config['eviction_policy']
+#        port             config['port']
+#        flush            config['flush']
+#        index_replica    config['index_replica']
+#        priority         config['priority']
+        install_path     node['couchbase']['server']['paths']['root']
+        cluster_username node['couchbase']['username']
+        cluster_password node['couchbase']['password']
+        cluster_port     node['couchbase']['port']
+        action :create
+    end
 end
