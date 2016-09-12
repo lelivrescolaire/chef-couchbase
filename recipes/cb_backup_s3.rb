2bfab1d7acded13c77bee865b933c82b94eecb83
Jérémy JOURDIN <jeremy.jourdin@free.fr>
2016-09-12 11:05:04 +0200

    Improve S3 backup command (variables)

--- recipes/cb_backup_s3.rb
+++ recipes/cb_backup_s3.rb
@@ -1,19 +1,22 @@
 unless node['couchbase']['backup']['s3']['bucket_name'].nil? || node['couchbase']['backup']['s3']['region'].nil?
     file = "/tmp/$(date +\\\%Y-\\\%m-\\\%d_\\\%H-\\\%M)"
 
+    variables = "FILE=#{file}"
+    variables = "#{variables} S3_FILE=$(date +\\\%Y)/$(date +\\\%m)/$(date +\\\%d)/$(date +\\\%H\\\%M)"
+
     cbBackupCommand = "#{node[:couchbase][:server][:paths][:root]}/bin/cbbackup -u #{node[:couchbase][:username]} -p #{node[:couchbase][:password]} --single-node"
     cbBackupCommand = "#{cbBackupCommand} http://localhost:#{node[:couchbase][:port]}"
-    cbBackupCommand = "#{cbBackupCommand} #{file}"
+    cbBackupCommand = "#{cbBackupCommand} $FILE"
 
-    zipCommand = "zip -r #{file}.zip #{file}"
+    zipCommand = "zip -r $FILE.zip $FILE"
 
     awsCommand = "aws s3 mv --region=#{node[:couchbase][:backup][:s3][:region]}"
-    awsCommand = "#{awsCommand} #{file}.zip"
-    awsCommand = "#{awsCommand} s3://#{node[:couchbase][:backup][:s3][:bucket_name]}#{node[:couchbase][:backup][:s3][:path]}/#{node[:opsworks][:instance][:hostname]}/$(date +\\\%Y)/$(date +\\\%m)/$(date +\\\%d)/$(date +\\\%H\\\%M).zip"
+    awsCommand = "#{awsCommand} $FILE.zip"
+    awsCommand = "#{awsCommand} s3://#{node[:couchbase][:backup][:s3][:bucket_name]}#{node[:couchbase][:backup][:s3][:path]}/#{node[:opsworks][:instance][:hostname]}/$S3_FILE.zip"
 
-    cleanCommand = "rm -rf #{file} #{file}.zip"
+    cleanCommand = "rm -rf $FILE $FILE.zip"
 
-    node.override['couchbase']['backup']['command'] = "#{cbBackupCommand} && #{zipCommand} && #{awsCommand} && #{cleanCommand}"
+    node.override['couchbase']['backup']['command'] = "#{variables}; #{cbBackupCommand} && #{zipCommand} && #{awsCommand} && #{cleanCommand}"
 
     include_recipe "couchbase::cb_backup"
 end
